f7d6cd2b0c5fe72c6c9ee63a5469bae45d7ca0d6
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-03 02:47:36 +0300

    complete

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -4,4 +4,10 @@ class ItemController < ApplicationController
         items.to_json()
     end
     #register an item
+get "/find_item" do
+    items= Item.all
+    items.to_json(include: {category:{only: [:name] }})
+end
+
+
 end
