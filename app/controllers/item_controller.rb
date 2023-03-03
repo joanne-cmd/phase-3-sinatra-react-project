cdc7e462e20ec77e39d5009ea3c959e84a0b0006
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-03 03:01:59 +0300

    added post

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -6,8 +6,29 @@ class ItemController < ApplicationController
     #register an item
 get "/find_item" do
     items= Item.all
-    items.to_json(include: {category:{only: [:name] }})
+    items.to_json(include: {Item:{only: [:name] }})
 end
 
+patch "/item/update/:id" do
+    items= Item.find_by(id: params[:id])
+    items.update(
+        name: params[:name],
+        image_url: params[:image_url],
+        description: params[:description]
+    )
+    items.to_json()
+ end
+ post "/fetch_items/" do
+    items= Item.create(
+        name: params[:name],
+        image_url: params[:image_url],
+        description: params[:description]
 
+    )
+    items.save()
+    {
+        "message": "created succesfull",
+        "status": "http 200"
+    }.to_json()
+end
 end
